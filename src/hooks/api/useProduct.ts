7ce756063a7809c50e367ff43adbaf89d3d13c52
alicezuberg930 <tien23851@gmail.com>
2025-08-08 11:25:59 -0700

    fix bug

--- src/hooks/api/useProduct.ts
+++ src/hooks/api/useProduct.ts
@@ -1,7 +1,6 @@
 import { useSnackbar } from "@/components/snackbar"
 import { API_PATH } from "@/routes/api"
 import { PATH_DASHBOARD } from "@/routes/paths"
-import { getProductListAPI } from "@/utils/httpClient"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { useCallback } from "react"
@@ -12,11 +11,11 @@ export default function useProduct() {
     const router = useRouter()
 
     const getProductList = useCallback(() => {
-        return useQuery({
-            queryKey: [API_PATH.product.list],
-            queryFn: () => getProductListAPI(),
-            placeholderData: (previousData, _) => previousData
-        })
+        // return useQuery({
+        //     queryKey: [API_PATH.product.list],
+        //     queryFn: () => getProductListAPI(),
+        //     placeholderData: (previousData, _) => previousData
+        // })
     }, [])
 
     const getProductDetails = useCallback((id: string) => {
